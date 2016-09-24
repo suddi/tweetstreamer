394f9a4187f69c3db47dc7d30faba723a55a998c
Sudharshan Ravindran <suddir@gmail.com>
2016-09-25 00:12:32 +0800

    Updated keywords

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from distutils.core import setup
 setup(
     name = 'tweetstreamer',
     packages = ['tweetstreamer'],
-    version = '1.0.0',
+    version = '1.0.1',
     description = 'A Tweepy streamer to collect tweets from Twitter\'s Streaming API based on criteria',
     license = 'MIT',
     author = 'Sudharshan Ravindran',
     author_email = 'suddir@gmail.com',
     url = 'https://github.com/suddi/tweetstreamer',
-    download_url = 'https://github.com/suddi/tweetstreamer/tarball/1.0.0',
-    keywords = ['twitter', 'streamer', 'streaming', 'api', 'tweet', 'tweets', 'tweepy'],
+    download_url = 'https://github.com/suddi/tweetstreamer/tarball/1.0.1',
+    keywords = ['twitter', 'streamer', 'streaming', 'api', 'tweet', 'tweets', 'tweepy', 'tweetstream', 'tweetstreamer'],
     classifiers = [],
 )
