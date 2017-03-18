cb014d06cf0f6ab1cff098048b3f038e1a985923
Sudharshan Ravindran <suddir@gmail.com>
2017-03-18 23:21:11 +0800

    Updated to publish wheel version

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name = 'tweetstreamer',
     packages = ['tweetstreamer'],
-    version = '1.0.3',
+    version = '1.0.4',
     description = 'A Tweepy streamer to collect tweets from Twitter\'s Streaming API based on criteria',
     license = 'MIT',
     author = 'Sudharshan Ravindran',
